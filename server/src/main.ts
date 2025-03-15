cc8bc51f6b0e80a82137306e891667f80de18925
Dryha Andriy <driga.and@gmail.com>
2025-03-15 17:59:06 +0200

    another deploy settings

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
+  const clientUrls = [process.env.CLIENT, process.env.CLIENT2];
+  console.log('clientUrls', clientUrls);
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors({
+    origin: clientUrls,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   /*app.enableCors({ origin: 'http://localhost:5173/' });*/
 
   await app.listen(PORT, () => console.log(`Server startet on port ${PORT}`));
